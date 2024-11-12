453d96da84a2d4e6d447b4fabce3e843a1c05f13
Osogi <dragonmail135159@gmail.com>
2024-11-12 12:56:07 +0300

    fix: fix not pretty prints for ast

--- BasedML/lib/ast/restoreSrc/test/test.ml
+++ BasedML/lib/ast/restoreSrc/test/test.ml
@@ -44,7 +44,7 @@ let%expect_test "Patterns" =
     | (x : int) -> 5);; |}]
 ;;
 
-let%expect_test "Expresions" =
+let%expect_test "Expressions" =
   test_restore_src
     {|
   let id = constant;;
@@ -64,7 +64,7 @@ let%expect_test "Expresions" =
     let  id = constant;;
     let  funct = (fun x -> x);;
     let  app = (funct id);;
-    let  ifthen = (if (((( = ) id) 2)) then true else (false));;
+    let  ifthen = (if ((( = ) id) 2) then true else false);;
     let  letinval = (let  a = 4 in ((( * ) a) 4));;
     let  tup = (1, []);;
     let  m = (match constant with
