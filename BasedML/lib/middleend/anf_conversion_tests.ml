fae64cdc4f0ae9bbbbb0381cb88c04504b3ba9e3
Pavel Averin <pasha2281337alfa@gmail.com>
2024-12-15 09:20:23 +0300

    test: rework tests for anf and add new ones

--- BasedML/lib/middleend/anf_conversion_tests.ml
+++ BasedML/lib/middleend/anf_conversion_tests.ml
@@ -2,374 +2,137 @@
 
 (** SPDX-License-Identifier: LGPL-2.1 *)
 
-open Ast
 open Anf
+open Restore_src.RestoreSrc
+open Typeinference
+open Typeinference__StartState
+
+let test_parse_and_convert_to_anf string_src =
+  let res = Parser.parse_program string_src in
+  match res with
+  | Result.Ok prog ->
+    Format.printf
+      "%s"
+      (restore_declarations (convert_declarations (Anf.anf_of_declarations prog)))
+  | Result.Error e -> Format.printf "Parser error: %s" e
+;;
+
+let parse_and_convert_to_anf string_src =
+  let res = Parser.parse_program string_src in
+  match res with
+  | Result.Ok prog ->
+    restore_declarations (convert_declarations (Anf.anf_of_declarations prog))
+  | Result.Error e -> e
+;;
+
+(* Expected anf programs *)
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "test"
-            , EApplication
-                ( EFunction
-                    ( PIdentifier "x"
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EIdentifier "x")
-                        , EConstant (CInt 1) ) )
-                , EConstant (CInt 2) ) ) )
-    ];
-  [%expect
+  test_parse_and_convert_to_anf
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "test"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-              (CApp (
-                 (ImmediateFunc ((PIdentifier "x"),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                       (CApp ((ImmediateIdentifier "( + )"),
-                          (ImmediateIdentifier "x"))),
-                       (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                          (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                             (ImmediateInt 1))),
-                          (ANFCEexpr
-                             (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                          ))
-                       ))
-                    )),
-                 (ImmediateInt 2))),
-              (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$3")))))
-           ))
-        ))
-      ] |}]
+    let x = 5|};
+  [%expect {| let  x = 5;; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( EApplication (EIdentifier "map", EIdentifier "lam")
-                , EApplication
-                    ( EApplication (EIdentifier "( :: )", EConstant (CInt 1))
-                    , EApplication
-                        ( EApplication (EIdentifier "( :: )", EConstant (CInt 2))
-                        , EApplication
-                            ( EApplication (EIdentifier "( :: )", EConstant (CInt 3))
-                            , EConstant CNil ) ) ) ) ) )
-    ];
-  [%expect
+  test_parse_and_convert_to_anf
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-              (CApp ((ImmediateIdentifier "map"), (ImmediateIdentifier "lam"))),
-              (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                 (CApp ((ImmediateIdentifier "( :: )"), (ImmediateInt 1))),
-                 (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-                    (CApp ((ImmediateIdentifier "( :: )"), (ImmediateInt 2))),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$4"),
-                       (CApp ((ImmediateIdentifier "( :: )"), (ImmediateInt 3))),
-                       (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$5"),
-                          (CApp ((ImmediateIdentifier "$ANF_VAR$4"), ImmediateNil
-                             )),
-                          (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$6"),
-                             (CApp ((ImmediateIdentifier "$ANF_VAR$3"),
-                                (ImmediateIdentifier "$ANF_VAR$5"))),
-                             (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$7"),
-                                (CApp ((ImmediateIdentifier "$ANF_VAR$2"),
-                                   (ImmediateIdentifier "$ANF_VAR$6"))),
-                                (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$8"),
-                                   (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                                      (ImmediateIdentifier "$ANF_VAR$7"))),
-                                   (ANFCEexpr
-                                      (CImmExpr
-                                         (ImmediateIdentifier "$ANF_VAR$8")))
-                                   ))
-                                ))
-                             ))
-                          ))
-                       ))
-                    ))
-                 ))
-              ))
-           ))
-        ))
-      ] |}]
+    let x = 5 + 6|};
+  [%expect
+    {| let  x = (let  anf_var1 = (( + ) 5) in (let  anf_var2 = (anf_var1 6) in anf_var2));; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EMatch
-                ( PIdentifier "x"
-                , [ ( PConstant (CInt 1)
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EConstant (CInt 2))
-                        , EConstant (CInt 3) ) )
-                  ; ( PConstant (CInt 2)
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EConstant (CInt 4))
-                        , EConstant (CInt 5) ) )
-                  ] ) ) )
-    ];
-  [%expect
+  test_parse_and_convert_to_anf
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFMatch ((PIdentifier "x"),
-              [((PConstant (CInt 1)),
-                (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                   (CApp ((ImmediateIdentifier "( + )"), (ImmediateInt 2))),
-                   (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                      (CApp ((ImmediateIdentifier "$ANF_VAR$1"), (ImmediateInt 3)
-                         )),
-                      (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))))
-                   )));
-                ((PConstant (CInt 2)),
-                 (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                    (CApp ((ImmediateIdentifier "( + )"), (ImmediateInt 4))),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                       (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                          (ImmediateInt 5))),
-                       (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                       ))
-                    )))
-                ]
-              ))
-           ))
-        ))
-      ] |}]
+    let x = 7 + 8 + 9|};
+  [%expect
+    {| let  x = (let  anf_var1 = (( + ) 7) in (let  anf_var2 = (anf_var1 8) in (let  anf_var3 = (( + ) anf_var2) in (let  anf_var4 = (anf_var3 9) in anf_var4))));; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( EFunction
-                    ( PIdentifier "x"
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EIdentifier "x")
-                        , EConstant (CInt 1) ) )
-                , EConstant (CInt 65) ) ) )
-    ];
+  test_parse_and_convert_to_anf
+    {|
+    let rec fac = fun n -> if n = 0 then 1 else n * (fac (n - 1))|};
   [%expect
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-              (CApp (
-                 (ImmediateFunc ((PIdentifier "x"),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                       (CApp ((ImmediateIdentifier "( + )"),
-                          (ImmediateIdentifier "x"))),
-                       (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                          (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                             (ImmediateInt 1))),
-                          (ANFCEexpr
-                             (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                          ))
-                       ))
-                    )),
-                 (ImmediateInt 65))),
-              (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$3")))))
-           ))
-        ))
-      ] |}]
+    let rec fac = (fun n -> (let  anf_var1 = (( = ) n) in (let  anf_var2 = (anf_var1 0) in (if (anf_var2) then 1 else ((let  anf_var3 = (( * ) n) in (let  anf_var4 = (( - ) n) in (let  anf_var5 = (anf_var4 1) in (let  anf_var6 = (fac anf_var5) in (let  anf_var7 = (anf_var3 anf_var6) in anf_var7))))))))));; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( EFunction
-                    ( PIdentifier "x"
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EIdentifier "x")
-                        , EConstant (CInt 1) ) )
-                , EConstant (CInt 65) ) ) )
-    ];
+  test_parse_and_convert_to_anf
+    {|
+    let fibo = fun n -> let rec fiboCPS = fun n acc -> match n with
+     | 0 -> acc 0
+     | 1 -> acc 1
+     | _ -> fiboCPS (n - 1) (fun x -> fiboCPS (n - 2) (fun y -> acc (x + y)))
+     in
+       fiboCPS n (fun x -> x)
+      |};
   [%expect
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-              (CApp (
-                 (ImmediateFunc ((PIdentifier "x"),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                       (CApp ((ImmediateIdentifier "( + )"),
-                          (ImmediateIdentifier "x"))),
-                       (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                          (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                             (ImmediateInt 1))),
-                          (ANFCEexpr
-                             (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                          ))
-                       ))
-                    )),
-                 (ImmediateInt 65))),
-              (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$3")))))
-           ))
-        ))
-      ] |}]
+    let  fibo = (fun n -> (let rec fiboCPS = (fun n -> (fun acc -> (match n with
+    | 0 -> (let  anf_var1 = (acc 0) in anf_var1)
+    | 1 -> (let  anf_var1 = (acc 1) in anf_var1)
+    | _ -> (let  anf_var1 = (( - ) n) in (let  anf_var2 = (anf_var1 1) in (let  anf_var3 = (fiboCPS anf_var2) in (let  anf_var5 = (anf_var3 (fun x -> (let  anf_var1 = (( - ) n) in (let  anf_var2 = (anf_var1 2) in (let  anf_var3 = (fiboCPS anf_var2) in (let  anf_var4 = (anf_var3 (fun y -> (let  anf_var1 = (( + ) x) in (let  anf_var2 = (anf_var1 y) in (let  anf_var3 = (acc anf_var2) in anf_var3))))) in anf_var4)))))) in anf_var5))))))) in (let  anf_var6 = (fiboCPS n) in (let  anf_var1 = (anf_var6 (fun x -> x)) in anf_var1))));; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( EConstant (CInt 65)
-                , EFunction
-                    ( PIdentifier "y"
-                    , EApplication
-                        ( EApplication (EIdentifier "( + )", EIdentifier "y")
-                        , EConstant (CInt 1) ) ) ) ) )
-    ];
+  test_parse_and_convert_to_anf {|let x = (5 + 6 + 7, 21 + 3, 30)|};
   [%expect
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-              (CApp ((ImmediateInt 65),
-                 (ImmediateFunc ((PIdentifier "y"),
-                    (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                       (CApp ((ImmediateIdentifier "( + )"),
-                          (ImmediateIdentifier "y"))),
-                       (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                          (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                             (ImmediateInt 1))),
-                          (ANFCEexpr
-                             (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                          ))
-                       ))
-                    ))
-                 )),
-              (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$3")))))
-           ))
-        ))
-      ] |}]
+    let  x = (let  anf_var2 = (( + ) 5) in (let  anf_var3 = (anf_var2 6) in (let  anf_var4 = (( + ) anf_var3) in (let  anf_var5 = (anf_var4 7) in (let  anf_var7 = (( + ) 21) in (let  anf_var8 = (anf_var7 3) in ((let  anf_var1 = anf_var5 in anf_var1), (let  anf_var6 = anf_var8 in anf_var6), (let  anf_var9 = 30 in anf_var9))))))));; |}]
 ;;
 
 let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( EFunction
-                    ( PIdentifier "x"
-                    , EApplication
-                        ( EApplication
-                            ( EIdentifier "( + )"
-                            , EApplication
-                                ( EApplication (EIdentifier "( + )", EIdentifier "x")
-                                , EConstant (CInt 1) ) )
-                        , EConstant (CInt 2) ) )
-                , EApplication
-                    ( EApplication (EIdentifier "( + )", EConstant (CInt 3))
-                    , EConstant (CInt 4) ) ) ) )
-    ];
+  test_parse_and_convert_to_anf {|let (x : int -> int) = 5 + 6 + 7|};
   [%expect
-    {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$5"),
-              (CApp ((ImmediateIdentifier "( + )"), (ImmediateInt 3))),
-              (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$6"),
-                 (CApp ((ImmediateIdentifier "$ANF_VAR$5"), (ImmediateInt 4))),
-                 (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$7"),
-                    (CApp (
-                       (ImmediateFunc ((PIdentifier "x"),
-                          (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                             (CApp ((ImmediateIdentifier "( + )"),
-                                (ImmediateIdentifier "x"))),
-                             (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                                (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                                   (ImmediateInt 1))),
-                                (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-                                   (CApp ((ImmediateIdentifier "( + )"),
-                                      (ImmediateIdentifier "$ANF_VAR$2"))),
-                                   (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$4"),
-                                      (CApp ((ImmediateIdentifier "$ANF_VAR$3"),
-                                         (ImmediateInt 2))),
-                                      (ANFCEexpr
-                                         (CImmExpr
-                                            (ImmediateIdentifier "$ANF_VAR$4")))
-                                      ))
-                                   ))
-                                ))
-                             ))
-                          )),
-                       (ImmediateIdentifier "$ANF_VAR$6"))),
-                    (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$7")))))
-                 ))
-              ))
-           ))
-        ))
-      ] |}]
+    {| let  (x : (int -> int)) = (let  anf_var1 = (( + ) 5) in (let  anf_var2 = (anf_var1 6) in (let  anf_var3 = (( + ) anf_var2) in (let  anf_var4 = (anf_var3 7) in anf_var4))));; |}]
 ;;
 
-let%expect_test _ =
-  test_prog
-    [ DSingleLet
-        ( NotRec
-        , DLet
-            ( PIdentifier "x"
-            , EApplication
-                ( ETuple
-                    [ EConstant (CInt 1)
-                    ; EApplication
-                        ( EApplication (EIdentifier "( + )", EConstant (CInt 2))
-                        , EConstant (CInt 6) )
-                    ; EConstant (CInt 3)
-                    ]
-                , EApplication
-                    ( EApplication (EIdentifier "( + )", EConstant (CInt 53))
-                    , EConstant (CInt 27) ) ) ) )
-    ];
-  [%expect
+(* Anf program should have the same type tests *)
+
+let%test _ =
+  let test_program = {|let  x = 5;;|} in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
+;;
+
+let%test _ =
+  let test_program = {|let x = 5 + 6;;|} in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
+;;
+
+let%test _ =
+  let test_program = {|let x = 7 + 8 + 9;;|} in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
+;;
+
+let%test _ =
+  let test_program =
     {|
-    [(ANFSingleLet (NotRec,
-        (ANFDec ((PIdentifier "x"),
-           (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-              (CApp ((ImmediateIdentifier "( + )"), (ImmediateInt 53))),
-              (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                 (CApp ((ImmediateIdentifier "$ANF_VAR$1"), (ImmediateInt 27))),
-                 (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$3"),
-                    (CApp (
-                       (ImmediateTuple
-                          [(ANFCEexpr (CImmExpr (ImmediateInt 1)));
-                            (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$1"),
-                               (CApp ((ImmediateIdentifier "( + )"),
-                                  (ImmediateInt 2))),
-                               (ANFLetIn (NotRec, (PIdentifier "$ANF_VAR$2"),
-                                  (CApp ((ImmediateIdentifier "$ANF_VAR$1"),
-                                     (ImmediateInt 6))),
-                                  (ANFCEexpr
-                                     (CImmExpr (ImmediateIdentifier "$ANF_VAR$2")))
-                                  ))
-                               ));
-                            (ANFCEexpr (CImmExpr (ImmediateInt 3)))]),
-                       (ImmediateIdentifier "$ANF_VAR$2"))),
-                    (ANFCEexpr (CImmExpr (ImmediateIdentifier "$ANF_VAR$3")))))
-                 ))
-              ))
-           ))
-        ))
-      ] |}]
+    let fibo = fun n -> let rec fiboCPS = fun n acc -> match n with
+     | 0 -> acc 0
+     | 1 -> acc 1
+     | _ -> fiboCPS (n - 1) (fun x -> fiboCPS (n - 2) (fun y -> acc (x + y)))
+     in
+       fiboCPS n (fun x -> x)
+      |}
+  in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
+;;
+
+let%test _ =
+  let test_program = {|let x = (5 + 6 + 7, 21 + 3, 30);;|} in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
+;;
+
+let%test _ =
+  let test_program = {|let (x : int) = 5 + 6 + 7;;|} in
+  test_infer_prog start_state test_program
+  = test_infer_prog start_state (parse_and_convert_to_anf test_program)
 ;;
